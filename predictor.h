377c7631f918d176725c577e4ede1f891ad28a83
Nick Trueb <ntrueb2@illinois.edu>
2021-11-30 15:40:26 -0600

    fixed whitespace

--- predictor.h
+++ predictor.h
@@ -4,12 +4,8 @@
 #include "utils.h"
 #include "tracer.h"
 #include <bitset>
-
 #define NUMTAGTABLES 4
 
-
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
 // Each entry in the tag Pred Table
 struct TagEntry 
 {
@@ -29,31 +25,6 @@ struct CompressedHist
       
     void updateCompHist(bitset<131> ghr)
     {
-        
-        /*bitset<131> temp;;
-        int numFolds = geomLength / targetLength;
-        //int remainder = (ghr.size()) % compLength;
-        int mask = (1 << targetLength) - 1;
-        int geometricMask = (1 << geomLength) - 1;
-        temp = ghr;
-        
-        temp = temp & geometricMask;
-        UINT32 remainder = 0;
-        UINT32 comphistfolds[numFolds];
-        
-        for(int i=0; i < numFolds; i++)
-        {    
-            comphistfolds[i] = temp & mask;
-            temp = temp >> targetLength;
-            
-        }
-        remainder = temp & mask;
-        
-        for(int i = 0; i< numFolds; i++)
-        {
-            compHist ^= comphistfolds[i];
-        }
-        compHist ^= remainder;*/
         // easier way below... see PPM paper Figure 2
         // creating important masks
         int mask = (1 << targetLength) - 1;
@@ -63,13 +34,11 @@ struct CompressedHist
             compHist ^= ((compHist & mask2) >> targetLength);
             compHist ^= mask1;
             compHist &= mask;  
-         
     }    
 };
 class PREDICTOR{
 
   // The state is defined for Gshare, change for your design
-
  private:
   bitset<131> GHR;           // global history register
   // 16 bit path history
@@ -102,19 +71,10 @@ class PREDICTOR{
 public:
 
   // The interface to the four functions below CAN NOT be changed
-
   PREDICTOR(void);
   bool    GetPrediction(UINT32 PC);  
   void    UpdatePredictor(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget);
   void    TrackOtherInst(UINT32 PC, OpType opType, UINT32 branchTarget);
-  
-  // Contestants can define their own functions below
-
 };
-
-
-  
-
-    /***********************************************************/
 #endif
 
